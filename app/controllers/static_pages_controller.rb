27ce67f54b277dbb9e4191e019bd2211d7279cb6
Zubair Desai <zubair.desai@gmail.com>
2013-10-17 17:05:09 -0700

    Make Static Pages DRY

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,19 @@
 class StaticPagesController < ApplicationController
   def home
+
+  	
   end
 
   def help
+  	
   end
+
+  def about
+
+  end
+
+  def contact
+
+  end
+
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "Home Page" do
+
+	  	it "should have the h1 'Sample App'" do
+	  		visit "/static_pages/home"
+	  		page.should have_selector("h1", text: "Sample App")
+	  	end
+
+	  	it "should have right title" do
+	  		visit "/static_pages/home"
+	  		page.should have_selector("title", text: "Sample App | Home")
+	  	end
+  end
+
+  describe "Help Page" do
+
+	  	it "should have the h1 'Help'" do
+	  		visit "/static_pages/help"
+	  		page.should have_selector("h1", text: "Help")
+	  	end
+
+	  	it "should have right title" do
+	  		visit "/static_pages/help"
+	  		page.should have_selector("title", text: "Sample App | Help")
+	  	end
+  end
+
+  describe "About Page" do
+
+	  	it "should have the h1 'About Us'" do
+	  		visit "/static_pages/about"
+	  		page.should have_selector("h1", text: "About Us")
+	  	end
+
+	  	it "should have right title" do
+	  		visit "/static_pages/about"
+	  		page.should have_selector("title", text: "Sample App | About Us")
+	  	end
+  end
+
+  describe "Contact Page" do
+
+	  	it "should have the h1 Contact Page" do
+	  		visit "/static_pages/contact"
+	  		page.should have_selector("h1", text: "Contact Page")
+	  	end
+
+	  	it "should have right title" do
+	  		visit "/static_pages/contact"
+	  		page.should have_selector("title", text: "Sample App | Contact")
+	  	end
+  end
+
+end
